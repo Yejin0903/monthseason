b6151a8f5609d73b7c766856b82fd4baf174e6d6
JUDY <s2018s05@e-mirim.hs.kr>
2020-05-06 23:33:44 +0900

    6. 계절 출력

--- monthseason/Program.cs
+++ monthseason/Program.cs
@@ -10,6 +10,34 @@ namespace monthseason
     {
         static void Main(string[] args)
         {
+            Console.Write("이번 달은 몇 월 인가요? : ");
+            int season = int.Parse(Console.ReadLine());
+            switch (season)
+            {
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("봄 입니다.");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("여름 입니다.");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("가을 입니다.");
+                    break;
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("겨울 입니다.");
+                    break;
+                default:
+                    Console.WriteLine("잘못 입력하셨습니다.");
+                    break;
+            }
         }
     }
 }
